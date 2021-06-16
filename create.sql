2e4801e3720e235433601d8afc2a9212560fb880
stiwanfrancis <84243224+stiwanfrancis@users.noreply.github.com>
2021-06-16 21:58:39 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,1 +1,23 @@
-
+CREATE TABLE publisher (
+    id INTEGER PRIMARY KEY,
+    name TEXT ,
+    country TEXT
+  );
+  
+CREATE TABLE books(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  publisher INT REFERENCES publisher(id)
+  );
+  
+CREATE TABLE subjects(
+  id INTEGER PRIMARY KEY,
+  name TEXT
+  );
+  
+CREATE TABLE books_subjects(
+  book INT REFERENCE books(id),
+  subject INT REFERENCE subjects(id)
+  );
+  
+  
